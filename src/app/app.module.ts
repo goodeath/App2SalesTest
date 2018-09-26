e7ce10078c641eb729962daa61e45ee909ef3d1e
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-26 18:29:30 -0300

    - Create component header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/admin/pages/login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HeaderComponent } from './modules/admin/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
