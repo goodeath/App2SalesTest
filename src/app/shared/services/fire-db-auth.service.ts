f60e2c12e33d858def4e2fb07b30ce67de349e4f
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 17:41:13 -0300

    Add shared firebase auth service.

---
+++ src/app/shared/services/fire-db-auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireDbAuthService {
+
+  private guest:boolean = true;
+
+  constructor( private db: AngularFireAuth, private router: Router) { }
+
+    public isGuest():boolean{
+      return this.guest
+    }
+
+    public authenticate(username: string, password: string) : Promise<firebase.auth.UserCredential | IAuthError> {
+      return this.db.auth.signInWithEmailAndPassword(username, password).then(
+         res => {
+           console.log(res);
+           this.guest = false;
+             return res;
+         }
+      ).catch( res => {
+
+        let error: IAuthError = res;
+        throw error;
+        
+      });
+    }
+
+    public logout():void{
+      this.guest = true;
+      this.db.auth.signOut();
+    }
+}
+
+export interface IAuthError {
+  code: string, 
+  message: string;
+}
