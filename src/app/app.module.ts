51d6f21e85394124383c0cbc164c106bc11c9326
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 17:43:27 -0300

    Add dependencies in app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LayoutModule } from 'angular-admin-lte';   //Import the layout module.
+import { CoreModule } from './core/core.module';
+import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
+import { BoxModule, BoxInfoModule as MkBoxInfoModule } from 'angular-admin-lte';
+import { AdminModule } from './modules/admin/admin.module';
+import { adminLteConf } from './admin-lte';   //Import the layout configuration.
+import { AngularFireModule } from 'angularfire2/index';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/admin/pages/login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HeaderComponent } from './modules/admin/components/header/header.component';
 import { SidebarComponent } from './modules/admin/components/sidebar/sidebar.component';
 import { FooterComponent } from './modules/admin/components/footer/footer.component';
 import { MainComponent } from './modules/admin/template/main/main.component';
-import { HomeComponent } from './modules/admin/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
     MainComponent,
-    HomeComponent
+   
   ],
   imports: [
+    ToastrModule.forRoot(), // Make toast through application.
+    BrowserAnimationsModule, // Toastr Animations
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    LayoutModule.forRoot(adminLteConf),   //Provide the configuration to the layout module.
+    LoadingPageModule, MaterialBarModule,
+    BoxModule, MkBoxInfoModule,
+    AdminModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
