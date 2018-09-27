26fc12ee0f885d007b296211152f42dcb572e4f3
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 19:07:00 -0300

    Add moment module in admin module.

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
 import { BoxModule, BoxInfoModule as MkBoxInfoModule } from 'angular-admin-lte';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
+import { MomentModule } from 'ngx-moment';
 
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
@@ -15,6 +16,7 @@ import { AngularFireDatabase } from 'angularfire2/database';
 
 @NgModule({
   imports: [
+    MomentModule,
     FormsModule,
     CommonModule,
     AdminRoutingModule,
