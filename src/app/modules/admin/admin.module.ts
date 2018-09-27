1e870eeb2e0c3625142540abfa3b7b14ac145540
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 17:51:16 -0300

    Add forms module to admin module

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
 import { BoxModule, BoxInfoModule as MkBoxInfoModule } from 'angular-admin-lte';
-
+import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
+
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
 import { AudioComponent } from './pages/audio/audio.component';
@@ -13,6 +14,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     AdminRoutingModule,
     LoadingPageModule, MaterialBarModule,
