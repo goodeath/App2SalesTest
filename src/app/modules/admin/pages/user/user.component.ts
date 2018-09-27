f57bc1a411caace3c7a610a5f6c5de7b93bd5053
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 19:06:46 -0300

    Show error when password doesn't match and success message.

--- src/app/modules/admin/pages/user/user.component.ts
+++ src/app/modules/admin/pages/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../../../../shared/services/user.service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -12,20 +14,34 @@ export class UserComponent implements OnInit {
     name: '',
     email: '',
     password: '',
-    confirm_password: '',
   }
+  public confirm_password  : string;
 
   public users:any[];
 
-  constructor(private user: UserService) { }
+  constructor(private user: UserService, private toastr: ToastrService) { }
 
   ngOnInit() {
     this.list();
   }
 
+  
+
   public create(){
     // console.log(this.user.getUsers());
-    this.user.add(this.data);
+    if(this.data.password != this.confirm_password){
+      this.toastr.error('Senhas não conferem');
+      return ;
+    }
+    this.user.add(this.data).then(res=>{
+      this.data = {
+        name: '',
+        email: '',
+        password: '',
+      };
+      delete(this.confirm_password)
+      this.toastr.success('Usuário criado com sucesso');
+    })
   }
 
   public list(){
