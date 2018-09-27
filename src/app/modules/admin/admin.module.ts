81bdf014b08c755c392f2adcc706d83673a22da9
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 17:43:08 -0300

    Add admin module

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
+import { BoxModule, BoxInfoModule as MkBoxInfoModule } from 'angular-admin-lte';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { AudioComponent } from './pages/audio/audio.component';
+
+import { FireDbAuthService } from './../../shared/services/fire-db-auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    LoadingPageModule, MaterialBarModule,
+    BoxModule, MkBoxInfoModule,
+  ],
+  declarations: [
+    HomeComponent,
+    UserComponent,
+    AudioComponent
+  ],
+  providers: [
+    FireDbAuthService,
+    AngularFireAuth
+  ]
+})
+export class AdminModule { }
