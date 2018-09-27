083a7e528efa07a9623d7f0f66b21ee2ec5cafa7
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 17:41:28 -0300

    Change login component to use firebase auth service.

--- src/app/modules/admin/pages/login/login.component.ts
+++ src/app/modules/admin/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { FireDbAuthService } from './../../../../shared/services/fire-db-auth.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,30 @@ import { Location } from '@angular/common';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor( private location: Location) { }
+  public email: string;
+  public password: string;
+  ToastrModule
+  constructor( private router: Router, private toast: ToastrService, private auth: FireDbAuthService) { }
 
   ngOnInit() {
   }
 
   public authenticate(): void{
-    this.location.go('Inicio');
+    
+    try{
+      this.auth.authenticate(this.email, this.password).then(res => {
+         console.log('lol',res);
+         this.router.navigate(['Admin/Inicio']);
+      }).catch( res => {
+        console.log('lol',res);
+        this.toast.error(res.message);
+         
+        //  this.toast.error(res['code']);
+      });
+    }catch(e){
+      this.toast.error(e.message);
+    }
   }
+
 
 }
