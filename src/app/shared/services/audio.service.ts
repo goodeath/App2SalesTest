43671c3cb8023c1c1d69c8fd5f9961aef790452d
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-28 06:09:44 -0300

    Add update and remove audio

--- src/app/shared/services/audio.service.ts
+++ src/app/shared/services/audio.service.ts
@@ -15,18 +15,28 @@ export class AudioService {
   }
 
   public upload(name: string, file: File){
-    this.storage.upload(name, file).then(res=>{
+    return this.storage.upload(name, file).then(res=>{
       console.log(res);
       let fullPath = res.ref.fullPath;
       let data = {
         date_created: moment().format('DD/MM/YYYY HH:mm:ss'),
         path: fullPath,
         name: name,
+        title: name
       };
-      this.db.insert(this.url.audio,data);
+      return this.db.insert(this.url.audio,data);
     }).catch(res => {
       console.log(res)
     })
+  }
+
+  public rm(uid: string){
+    let fullPath = this.url.audio + '/' + uid;
+    return this.db.delete(fullPath);
+  }
+
+  public update(name: string, uid:string){
+    return this.db.set(this.url.audio + '/' + uid + '/title',name);
   }
 
   public list(){
