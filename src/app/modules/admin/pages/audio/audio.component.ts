a484fc7ebfc209e665c64899f3ff8dc4ff144431
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 21:34:17 -0300

    Add audio services to audio component.

--- src/app/modules/admin/pages/audio/audio.component.ts
+++ src/app/modules/admin/pages/audio/audio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AudioService } from './../../../../shared/services/audio.service';
 @Component({
   selector: 'app-audio',
   templateUrl: './audio.component.html',
@@ -7,16 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudioComponent implements OnInit {
 
-  public file;
-  constructor() { }
+  public file: File;
+  public name: string;
+  public audios: any[];
+  constructor(private audio: AudioService) { }
 
   ngOnInit() {
+    this.list();
   }
 
   public watchFile(event){
     console.log(event);
+    this.file = event.target.files[0];
   }
   public save(){
+    this.audio.upload(this.name,this.file);
     console.log(this.file);
+  
+  }
+
+  public list(){
+    console.log(this.audio.getAudios());
+    this.audio.getAudios().subscribe(res=>{
+      this.audios = res;
+      console.log(res);
+    });
   }
 }
