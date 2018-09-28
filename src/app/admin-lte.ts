a86d7912a8dd3506c18e46470f25582d1aa5f14f
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-28 02:14:37 -0300

    Update admin-lte routes.

--- src/app/admin-lte.ts
+++ src/app/admin-lte.ts
@@ -2,9 +2,9 @@ export var adminLteConf = {
   skin: 'blue',
   sidebarLeftMenu: [
     {label: 'Painel', separator: true},
-    {label: 'Dashboard', route: '/Inicio', iconClasses: 'fa fa-line-chart'},
-    {label: 'Ingressos', route: '/Ingressos', iconClasses: 'fa fa-ticket'},
-    {label: 'Áudios', route: '/Audios', iconClasses: 'fa fa-volume-up'},
+    {label: 'Dashboard', route: '/Admin/Inicio', iconClasses: 'fa fa-line-chart'},
+    {label: 'Ingressos', route: '/Admin/Ingressos', iconClasses: 'fa fa-ticket'},
+    {label: 'Áudios', route: '/Admin/Audios', iconClasses: 'fa fa-volume-up'},
     {
       label: 'Usuários', route: '/Admin/Usuarios', iconClasses: 'fa fa-user'},
     {label: 'Logout', separator: true},
