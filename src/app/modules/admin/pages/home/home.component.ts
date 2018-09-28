27356f2d0cea6a1b8042b54bdb7ab9f3ba7df362
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-28 03:33:48 -0300

    Update info in dashboard

--- src/app/modules/admin/pages/home/home.component.ts
+++ src/app/modules/admin/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from './../../../../shared/services/user.service';
+import { AudioService } from './../../../../shared/services/audio.service';
+import { TicketService } from './../../../../shared/services/ticket.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public users: any[] = null;
+  public audios: any[] = null;
+  public tickets: any[] = null;
+  constructor(
+    private user: UserService,
+    private audio: AudioService,
+    private ticket: TicketService) { }
 
   ngOnInit() {
+    this.listUsers();
+    this.listAudios();
+    this.listTickets();
+  }
+
+  private listUsers():void{
+    this.user.getUsers().subscribe(res=>{
+      this.users = res;
+    })
+  }
+
+  private listAudios():void{
+    this.audio.getAudios().subscribe(res=>{
+      this.audios = res;
+    })
+  }
+
+  private listTickets():void{
+    this.ticket.getTickets().subscribe(res=>{
+      this.tickets = res;
+    })
   }
 
 }
