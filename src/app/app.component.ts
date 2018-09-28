acdb65d44600dde0b0e88082ef53b7c863edf452
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-28 03:37:01 -0300

    Add auth middleware

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from 'angular-admin-lte';
-
+import { Router } from '@angular/router';
+import { FireDbAuthService } from './shared/services/fire-db-auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -9,10 +10,14 @@ export class AppComponent implements OnInit {
   public customLayout: boolean;
 
   constructor(
-    private layoutService: LayoutService
+    private layoutService: LayoutService,
+    private router: Router,
+    private auth: FireDbAuthService,
   ) {}
 
   ngOnInit() {
+    if(this.auth.isGuest())
+      this.router.navigate(['/Entrar']);
     this.layoutService.isCustomLayout.subscribe((value: boolean) => {
       this.customLayout = value;
     });
