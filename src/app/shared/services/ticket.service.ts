b5872f148b1dee0f343322f44c9c7122bd5205af
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-27 21:56:20 -0300

    - Add Routes to ticket. - Add create method.

--- src/app/shared/services/ticket.service.ts
+++ src/app/shared/services/ticket.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { FireDbService } from './fire-db.service'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
 
-  constructor() { }
+  private url = {
+     'ticket':'/tickets',
+     'schedule':'/tickets_schedule',
+     'sell' : '/tickets_sell/:id',
+     'sellUser':'/tickets_sell_user/',
+     'graph' : '/tickets_month_year'
+  }
+
+  constructor(private db: FireDbService) { }
+
+  public create(name: string, description: string, benefits: any[]){
+    let data = {
+      'name':name,
+      'description':description,
+      'beneftis': benefits
+    };
+    this.db.insert(this.url.ticket,data).then(res=>{
+      return res;
+    })
+  }
+
 }
