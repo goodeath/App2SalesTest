c0230ba90af399ebb1d295626585e74b6056dc12
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-30 16:52:54 -0300

    Get file percent upload.

--- src/app/shared/services/fire-db-storage.service.ts
+++ src/app/shared/services/fire-db-storage.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FireDbStorageService {
 
+  public uploadPercent: Observable<number>;
+
   constructor(private db: AngularFireStorage) { }
 
   public upload (name: string, file): Promise<any> {
     let ref = this.db.ref('audios/' + name + '.mp3');
-    return ref.put(file).then((snapshot)=>{
+    let task =  ref.put(file)
+    this.uploadPercent = task.percentageChanges();
+    return task.then((snapshot)=>{
       return snapshot;
     })
     
