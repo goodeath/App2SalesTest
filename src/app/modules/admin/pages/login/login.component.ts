09141a339c7a06f193f0734250d13747956cd2f8
Alex Anderson <alexandersonbm@hotmail.com>
2018-09-30 16:50:18 -0300

    Add Loading in DB routines.

--- src/app/modules/admin/pages/login/login.component.ts
+++ src/app/modules/admin/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FireDbAuthService } from './../../../../shared/services/fire-db-auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { LoadingService } from '../../../../shared/services/loading.service';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
   public email: string;
   public password: string;
   ToastrModule
-  constructor( private router: Router, private toast: ToastrService, private auth: FireDbAuthService) { }
+  constructor( private router: Router, private toast: ToastrService, private auth: FireDbAuthService, private loading: LoadingService) { }
 
   private errors = {
     'auth/argument-error' : 'E-mail e senha precisam estar preenchidos',
@@ -27,18 +28,25 @@ export class LoginComponent implements OnInit {
   }
 
   public authenticate(): void{
-    
+ 
+    console.log();
+ 
+    let open = this.loading.open("Realizando Autenticação!");
     try{
+      
       this.auth.authenticate(this.email, this.password).then(res => {
+        open.finish();
          console.log('lol',res);
          this.router.navigate(['Admin/Inicio']);
       }).catch( res => {
+        open.finish();
         console.log('lol',res);
         this.toast.error(this.errors[res.code]);
          
         //  this.toast.error(res['code']);
       });
     }catch(e){
+      open.finish();
       console.log(e);
       this.toast.error(this.errors[e.code]);
     }
