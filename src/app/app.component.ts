39176268b7c3d39c5064e14c074f0abedc0b339b
Alex Anderson <alexandersonbm@hotmail.com>
2018-10-01 04:03:18 -0300

    Set theme ngx bs3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LayoutService } from 'angular-admin-lte';
 import { Router } from '@angular/router';
 import { FireDbAuthService } from './shared/services/fire-db-auth.service';
+import { setTheme } from 'ngx-bootstrap/utils';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -16,8 +18,9 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    if(this.auth.isGuest())
-      this.router.navigate(['/Entrar']);
+    setTheme('bs3'); // or 'bs4'
+    // if(this.auth.isGuest())
+    //   this.router.navigate(['/Entrar']);
     this.layoutService.isCustomLayout.subscribe((value: boolean) => {
       this.customLayout = value;
     });
