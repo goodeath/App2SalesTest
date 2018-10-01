f481d442b15987f9e90e228843ce2be2808115a4
Alex Anderson <alexandersonbm@hotmail.com>
2018-10-01 03:58:05 -0300

    Add donut chart component

---
+++ src/app/shared/components/donut-chart/donut-chart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-chart',
+  templateUrl: './donut-chart.component.html',
+  styleUrls: ['./donut-chart.component.css']
+})
+export class DonutChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Web', 'Mobile'];
+  public doughnutChartData:number[] = [350, 450];
+  public doughnutChartType:string = 'doughnut';
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+}
